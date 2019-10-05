6a139570368e7c7a7d7cd5dfeaa61303ed00ddcc
Pasindu Sandeepa <38062478+PasinduSan@users.noreply.github.com>
2019-10-05 22:26:13 +0530

    pasindu sandeepa

---
+++ Variables/variablesps.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+    int i2;
+    int xps;
+    double d2;
+    char s2[100]; // this is not scalable for input of unknown size
+
+    // Read inputs from stdin(scanf)
+    scanf("%d", &i2);
+    scanf("%d", &xps);
+    scanf("%lf", &d2);
+    scanf("%*[\n] %[^\n]", s2);
+
+    // Print outputs to stdout(printf)
+    printf("%d\n", i + i2);
+    printf("%d\n", i + xps);
+    printf("%0.1lf\n", d + d2);
+    printf("%s%s", s, s2);
+
+    return 0;
