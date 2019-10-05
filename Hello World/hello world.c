5cc0e72d8adaceaca9506c48662939027c1cf8fc
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-10-05 20:36:13 +0530

    Hello World

---
+++ Hello World/hello world.c
@@ -0,0 +1,18 @@
+ //hacktoberfest2019
+//oshada97
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char input_string[100];
+    
+    scanf("%[^\n]s",&input_string);
+    printf("Hello, World.\n");
+    printf("%s",input_string);
+    return 0;
+}
